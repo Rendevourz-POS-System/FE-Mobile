a367d54f7bb46b1e8da89f14af33d9a48d632aab
crisl <reavleaz@yahoo.co.id>
2024-03-06 22:18:49 +0700

    style : add bottom tab to home screen

--- components/navigations/Root/RootNavigationStack.tsx
+++ components/navigations/Root/RootNavigationStack.tsx
@@ -1,16 +1,18 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NativeStackNavigationOptions, createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from 'react';
 import { RootNavigationStackParams } from "./RootNavigationStackParams";
-import HomeScreen from "./screens/components/HomeScreen";
-
-const stack = createNativeStackNavigator<RootNavigationStackParams>();
+import RootBottomTab from "../RootBottomTab/RootBottomTab"
+const Stack = createNativeStackNavigator<RootNavigationStackParams>();
 
 const RootNavigationStack : React.FC = () => {
     return (
-        <stack.Navigator>
-            <stack.Screen name="Home" component={HomeScreen} />
-        </stack.Navigator>
+        <Stack.Navigator>
+            <Stack.Screen name="TabMenu" component={RootBottomTab} options={noHeader}/>
+        </Stack.Navigator>
     )
 }
+const noHeader: NativeStackNavigationOptions = {
+    headerShown: false,
+};
 
 export default RootNavigationStack;
