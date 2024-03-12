1c743b266a1e8c5f59e21414378e1e11b98fb38a
crisl <reavleaz@yahoo.co.id>
2024-03-12 15:55:00 +0700

    Add scrollview to login and register screen

--- components/navigations/Root/screens/LoginScreen.tsx
+++ components/navigations/Root/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from "react";
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { CheckBox, Icon } from "react-native-elements";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { RootNavigationStackScreenProps } from "../../StackScreenProps";
@@ -20,60 +20,63 @@ export const LoginScreen: FC<RootNavigationStackScreenProps<'LoginScreen'>> = ({
 
     return (
         <SafeAreaProvider className="flex-1 bg-white">
-            <View className="my-8">
-                <View className="items-center my-10">
-                    <Text className="text-2xl mb-5">Welcome To Our App</Text>
-                    <Image source={require('../../../../assets/logo-login.png')} />
-                </View>
+            <ScrollView>
 
-                <View style={style.inputBox} className="mb-8">
-                    <TextInput
-                        placeholder="Email"
-                    />
-                </View>
+                <View className="my-8">
+                    <View className="items-center my-10">
+                        <Text className="text-2xl mb-5">Welcome To Our App</Text>
+                        <Image source={require('../../../../assets/logo-login.png')} />
+                    </View>
 
-                <View style={style.inputBox}>
-                    <TextInput
-                        placeholder="Password"
-                        style={{flex: 1}}
-                    />
-                    <TouchableOpacity onPress={togglePasswordVisibility} style={style.passwordToggleIcon}>
-                        <Icon name={showPassword ? 'eye-slash' : 'eye'} type="font-awesome" size={18} color="#666" />
+                    <View style={style.inputBox} className="mb-8">
+                        <TextInput
+                            placeholder="Email"
+                        />
+                    </View>
+
+                    <View style={style.inputBox}>
+                        <TextInput
+                            placeholder="Password"
+                            style={{flex: 1}}
+                        />
+                        <TouchableOpacity onPress={togglePasswordVisibility} style={style.passwordToggleIcon}>
+                            <Icon name={showPassword ? 'eye-slash' : 'eye'} type="font-awesome" size={18} color="#666" />
+                        </TouchableOpacity>
+                    </View>
+
+                    <View className="flex-row justify-between mx-8 top-2">
+                        <CheckBox
+                            checked={rememberMe}
+                            onPress={handleRememberMeChange}
+                            checkedColor="#488DF4"
+                            containerStyle={style.checkboxContent}
+                        ></CheckBox>
+                        <Text style={style.fontColor} className="mt-1">Remember Me</Text>
+                        <TouchableOpacity>
+                            <Text style={style.fontColor} className="mt-1">Forgot Password?</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <TouchableOpacity style={style.button}>
+                        <Text className="text-center font-bold text-white">Sign In</Text>
                     </TouchableOpacity>
+
+                    <Text className="text-center my-5 opacity-30">Or Sign In With</Text>
+
+                    <View className="flex-row flex justify-evenly">
+                        <Image source={require('../../../../assets/Facebook.png')} />
+                        <Image source={require('../../../../assets/Google.png')} />
+                        <Image source={require('../../../../assets/Instagram.png')} />
+                    </View>
+
+                    <View className="flex-row justify-center top-5">
+                        <Text>Don't have an account?</Text>
+                        <TouchableOpacity onPress={() => navigation.navigate("RegisterScreen")}>
+                            <Text className="underline underline-offset-4" style={style.fontColor}> Sign Up</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-
-                <View className="flex-row justify-between mx-8 top-2">
-                    <CheckBox
-                        checked={rememberMe}
-                        onPress={handleRememberMeChange}
-                        checkedColor="#488DF4"
-                        containerStyle={style.checkboxContent}
-                    ></CheckBox>
-                    <Text style={style.fontColor} className="mt-1">Remember Me</Text>
-                    <TouchableOpacity>
-                        <Text style={style.fontColor} className="mt-1">Forgot Password?</Text>
-                    </TouchableOpacity>
-                </View>
-
-                <TouchableOpacity style={style.button}>
-                    <Text className="text-center font-bold text-white">Sign In</Text>
-                </TouchableOpacity>
-
-                <Text className="text-center my-5 opacity-30">Or Sign In With</Text>
-
-                <View className="flex-row flex justify-evenly">
-                    <Image source={require('../../../../assets/Facebook.png')} />
-                    <Image source={require('../../../../assets/Google.png')} />
-                    <Image source={require('../../../../assets/Instagram.png')} />
-                </View>
-
-                <View className="flex-row justify-center top-5">
-                    <Text>Don't have an account?</Text>
-                    <TouchableOpacity onPress={() => navigation.navigate("RegisterScreen")}>
-                        <Text className="underline underline-offset-4" style={style.fontColor}> Sign Up</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+            </ScrollView>
         </SafeAreaProvider>
     )
 }
--- components/navigations/Root/screens/RegisterScreen.tsx
+++ components/navigations/Root/screens/RegisterScreen.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from "react";
 import { RootNavigationStackScreenProps } from "../../StackScreenProps";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { View, Text, Image, TextInput, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, Image, TextInput, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
 import { Icon } from "react-native-elements";
 
 export const RegisterScreen: FC<RootNavigationStackScreenProps<'RegisterScreen'>> = ({ navigation }) => {
@@ -13,55 +13,60 @@ export const RegisterScreen: FC<RootNavigationStackScreenProps<'RegisterScreen'>
     
     return (
         <SafeAreaProvider className="flex-1 bg-white">
-            <View className="my-8">
-                <View className="items-center my-10">
-                    <Text className="text-2xl mb-5">Sign Up</Text>
-                    <Image source={require('../../../../assets/logo-register.png')} />
-                </View>
+            <ScrollView>
+                <View className="mb-8">
+                    <View className="items-center my-10">
+                        <View className="absolute left-7 top-1">
+                            <Icon name="arrow-left" type="font-awesome" onPress={() => navigation.goBack()} />
+                        </View>
+                        <Text className="text-2xl mb-5">Sign Up</Text>
+                        <Image source={require('../../../../assets/logo-register.png')} />
+                    </View>
 
-                <View style={style.inputBox}>
-                    <TextInput
-                        placeholder="Name"
-                    />
-                </View>
+                    <View style={style.inputBox}>
+                        <TextInput
+                            placeholder="Name"
+                        />
+                    </View>
 
-                <View style={style.inputBox}>
-                    <TextInput
-                        placeholder="Email"
-                    />
-                </View>
+                    <View style={style.inputBox}>
+                        <TextInput
+                            placeholder="Email"
+                        />
+                    </View>
 
-                <View style={style.inputBox}>
-                    <TextInput
-                        placeholder="Password"
-                        style={{flex: 1}}
-                    />
-                    <TouchableOpacity onPress={togglePasswordVisibility} style={style.passwordToggleIcon}>
-                        <Icon name={showPassword ? 'eye-slash' : 'eye'} type="font-awesome" size={18} color="#666" />
+                    <View style={style.inputBox}>
+                        <TextInput
+                            placeholder="Password"
+                            style={{flex: 1}}
+                        />
+                        <TouchableOpacity onPress={togglePasswordVisibility} style={style.passwordToggleIcon}>
+                            <Icon name={showPassword ? 'eye-slash' : 'eye'} type="font-awesome" size={18} color="#666" />
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={style.inputBox}>
+                        <TextInput
+                            placeholder="Confirm Password"
+                            style={{flex: 1}}
+                        />
+                        <TouchableOpacity onPress={togglePasswordVisibility} style={style.passwordToggleIcon}>
+                            <Icon name={showPassword ? 'eye-slash' : 'eye'} type="font-awesome" size={18} color="#666" />
+                        </TouchableOpacity>
+                    </View>
+
+                    <TouchableOpacity style={style.button}>
+                        <Text className="text-center font-bold text-white">Sign In</Text>
                     </TouchableOpacity>
-                </View>
 
-                <View style={style.inputBox}>
-                    <TextInput
-                        placeholder="Confirm Password"
-                        style={{flex: 1}}
-                    />
-                    <TouchableOpacity onPress={togglePasswordVisibility} style={style.passwordToggleIcon}>
-                        <Icon name={showPassword ? 'eye-slash' : 'eye'} type="font-awesome" size={18} color="#666" />
-                    </TouchableOpacity>
+                    <View className="flex-row justify-center top-5">
+                        <Text>Already have an account?</Text>
+                        <TouchableOpacity onPress={() => navigation.navigate("LoginScreen")}>
+                            <Text className="underline underline-offset-4" style={style.fontColor}> Sign In</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-
-                <TouchableOpacity style={style.button}>
-                    <Text className="text-center font-bold text-white">Sign In</Text>
-                </TouchableOpacity>
-
-                <View className="flex-row justify-center top-5">
-                    <Text>Already have an account?</Text>
-                    <TouchableOpacity onPress={() => navigation.navigate("LoginScreen")}>
-                        <Text className="underline underline-offset-4" style={style.fontColor}> Sign In</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+            </ScrollView>
         </SafeAreaProvider>
     )
 }
