e427badc86f1469bcfa889e55ec26260de2e931d
vasthytanisya <vrezkitanisya26@gmail.com>
2024-04-22 20:46:16 +0700

    Get User Data

--- components/navigations/Profile/screens/ManageScreen.tsx
+++ components/navigations/Profile/screens/ManageScreen.tsx
@@ -1,68 +1,80 @@
 import { Ionicons } from "@expo/vector-icons";
-import React, { FC, useState } from "react";
-import { ScrollView, Text, View, StyleSheet, TouchableOpacity, TextInput, Image } from "react-native";
+import React, { FC, useEffect, useState } from "react";
+import { ScrollView, Text, View, StyleSheet, TouchableOpacity, TextInput, Alert } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ProfileRootBottomTabCompositeScreenProps } from "../../CompositeNavigationProps";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from 'react-hook-form';
 import { FontAwesome6 } from '@expo/vector-icons';
-import * as ImagePicker from 'expo-image-picker';
+import { BackendApiUri } from "../../../../functions/BackendApiUri";
+import axios from "axios";
+
+interface User {
+    Username: string,
+    Email: string;
+    Address: string,
+    City: string,
+    Nik: string,
+    PhoneNumber: string,
+    PostalCode: number,
+    Province: string,
+}
 
 const profileFormSchema = z.object({
-    name: z.string({ required_error: "Name cannot be empty" }).min(5, { message: "Name must be more than 5 character" }),
-    email: z.string({ required_error: "Email cannot be empty" }).email({ message: "Invalid email address" }),
-    phoneNumber: z.string({ required_error: "Phone Number cannot be empty" }).min(1, { message: "Phone number cannot be empty" }).refine(value => /^\d+$/.test(value), { message: "Phone Number must be numeric (0-9)" }),
-    address: z.string({ required_error: "Address cannot be empty" }).min(1, { message: "Address cannot be empty" }),
-    password: z.string({ required_error: "Password cannot be empty" })
+    Nik: z.string({ required_error: "NIK tidak boleh kosong" }).regex(/^\d{16}$/, { message: "Format NIK tidak valid" }),
+    PhoneNumber: z.string({ required_error: "Nomor telepon tidak boleh kosong" }).min(1, { message: "Nomor Telepon tidak boleh kosong" }).refine(value => /^\d+$/.test(value), { message: "Nomor telepon harus berupa angka (0-9)" }),
+    Address: z.string({ required_error: "Alamat tidak boleh kosong" }).min(1, { message: "Alamat tidak boleh kosong" }),
+    City: z.string({ required_error: "Kabupaten/Kota tidak boleh kosong" }).min(1, { message: "Kabupaten/Kota tidak boleh kosong" }),
+    Province: z.string({ required_error: "Provinsi tidak boleh kosong" }).min(1, { message: "Provinsi tidak boleh kosong" }),
+    PostalCode: z.number({ required_error: 'Kode pos tidak boleh kosong' }),
 })
 
 type ProfileFormType = z.infer<typeof profileFormSchema>
 
 export const ManageScreen: FC<ProfileRootBottomTabCompositeScreenProps<'ManageScreen'>> = ({ navigation }) => {
-    const [image, setImage] = useState('');
+    const [userData, setUserData] = useState<User>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${BackendApiUri.getUserData}`);
+                setUserData(response.data);
+                setValue("Nik", response.data?.Nik);
+                setValue("PhoneNumber", response.data?.PhoneNumber);
+                setValue("Address", response.data?.Address);
+                setValue("City", response.data?.City);
+                setValue("Province", response.data?.Province);
+                setValue("PostalCode", response.data?.PostalCode);
+            } catch (error) {
+                console.error("Error fetching shelter data:", error);
+            }
+        };
+        fetchData();
+    }, []);
 
     const {
         control,
-        watch,
         setValue,
         handleSubmit,
         formState: { errors },
     } = useForm<ProfileFormType>({
-        resolver: zodResolver(profileFormSchema),
-        defaultValues: {
-            name: "John",
-            email: "JohnDue@gmail.com",
-            phoneNumber: "",
-            address: "",
-            password: "******",
-        }
+        resolver: zodResolver(profileFormSchema)
     });
 
-    const pickImage = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            aspect: [1, 1],
-            quality: 1,
-        })
-
-        if (!result.canceled) {
-            setImage(result.assets[0].uri)
-        }
-    };
-
     const onSubmit = async (data: ProfileFormType) => {
-        setValue("name", data.name);
-        setValue("email", data.email);
-        setValue("phoneNumber", data.phoneNumber);
-        setValue("address", data.address);
-        console.log(data);
+        setValue("Nik", data.Nik);
+        setValue("PhoneNumber", data.PhoneNumber);
+        setValue("Address", data.Address);
+        setValue("City", data.City);
+        setValue("Province", data.Province);
+        setValue("PostalCode", data.PostalCode);
+        Alert.alert('Data Tersimpan', 'Data anda telah tersimpan.');
     }
 
     return (
         <SafeAreaProvider style={styles.container}>
-            <View className="mt-14 flex-row items-center justify-center">
+            <View className="mt-14 flex-row items-center justify-center mb-3">
                 <Ionicons name="chevron-back" size={24} color="black" onPress={() => navigation.goBack()} style={{ position: 'absolute', left: 20 }} />
                 <Text className="text-xl">Manage Profile</Text>
             </View>
@@ -70,99 +82,136 @@ export const ManageScreen: FC<ProfileRootBottomTabCompositeScreenProps<'ManageSc
             <ScrollView>
                 <View className="mb-10 mt-10">
                     <View style={styles.rowContainer} className="justify-around">
-                        <TouchableOpacity
-                            style={{ width: 100, height: 100, backgroundColor: '#2E3A59', borderRadius: 50, justifyContent: 'center', alignItems: 'center' }}
-                            onPress={pickImage}
-                            disabled={image ? true : false}
-                        >
-                            {image ? (<Image source={{ uri: image }} style={{ width: 100, height: 100, borderRadius: 50 }} />) : (<Ionicons name="camera" size={40} color="white" />)}
-                        </TouchableOpacity>
-                        <Text className="top-5 text-3xl">David Robinson</Text>
+                        <Ionicons name="person-circle-outline" size={120} color="black" />
+                        <Text className="top-5 text-3xl">{userData?.Username}</Text>
                     </View>
                 </View>
 
-                <View style={styles.inputBox}>
-                    <Controller
-                        name="name"
-                        control={control}
-                        render={({ field: { value } }) => (
-                            <TextInput
-                                style={{ flex: 1 }}
-                                placeholder="Name"
-                                onChangeText={(text: string) => setValue('name', text)}
-                                value={value}
-                            />
-                        )}
+                <View style={styles.disabledInput}>
+                    <TextInput
+                        style={{ flex: 1 }}
+                        placeholder="Name"
+                        value={userData?.Username}
+                        editable={false}
                     />
-                    <FontAwesome6 name="edit" size={24} color="black" />
                 </View>
-                <Text style={styles.errorMessage}>{errors.name?.message}</Text>
-
-                <View style={styles.inputBox}>
-                    <Controller
-                        name="email"
-                        control={control}
-                        render={({ field: { value } }) => (
-                            <TextInput
-                                style={{ flex: 1 }}
-                                placeholder="Email"
-                                onChangeText={(text: string) => setValue('email', text)}
-                                value={value}
-                            />
-                        )}
-                    />
-                    <FontAwesome6 name="edit" size={24} color="black" />
-                </View>
-                <Text style={styles.errorMessage}>{errors.email?.message}</Text>
-
-                <View style={styles.inputBox}>
-                    <Controller
-                        name="phoneNumber"
-                        control={control}
-                        render={({ field: { value } }) => (
-                            <TextInput
-                                style={{ flex: 1 }}
-                                placeholder="Phone Number"
-                                onChangeText={(text: string) => setValue('phoneNumber', text)}
-                                value={value}
-                            />
-                        )}
-                    />
-                    <FontAwesome6 name="edit" size={24} color="black" />
-                </View>
-                <Text style={styles.errorMessage}>{errors.phoneNumber?.message}</Text>
-
-                <View style={styles.inputBox}>
-                    <Controller
-                        name="address"
-                        control={control}
-                        render={({ field: { value } }) => (
-                            <TextInput
-                                style={{ flex: 1 }}
-                                placeholder="Address"
-                                onChangeText={(text: string) => setValue('address', text)}
-                                value={value}
-                            />
-                        )}
-                    />
-                    <FontAwesome6 name="edit" size={24} color="black" />
-                </View>
-                <Text style={styles.errorMessage}>{errors.address?.message}</Text>
 
                 <View style={styles.disabledInput}>
+                    <TextInput
+                        style={{ flex: 1 }}
+                        placeholder="Email"
+                        value={userData?.Email}
+                        editable={false}
+                    />
+                </View>
+
+                <View style={styles.inputBox}>
                     <Controller
-                        name="password"
+                        name="Nik"
                         control={control}
                         render={({ field: { value } }) => (
                             <TextInput
                                 style={{ flex: 1 }}
-                                placeholder="Password"
+                                placeholder="NIK"
+                                onChangeText={(text: string) => setValue('Nik', text)}
                                 value={value}
-                                editable={false}
                             />
                         )}
                     />
+                    <FontAwesome6 name="edit" size={24} color="black" />
                 </View>
+                <Text style={styles.errorMessage}>{errors.Nik?.message}</Text>
+
+                <View style={styles.inputBox}>
+                    <Controller
+                        name="PhoneNumber"
+                        control={control}
+                        render={({ field: { value } }) => (
+                            <TextInput
+                                style={{ flex: 1 }}
+                                placeholder="Nomor Telepon"
+                                onChangeText={(text: string) => setValue('PhoneNumber', text)}
+                                value={value}
+                            />
+                        )}
+                    />
+                    <FontAwesome6 name="edit" size={24} color="black" />
+                </View>
+                <Text style={styles.errorMessage}>{errors.PhoneNumber?.message}</Text>
+
+                <View style={styles.inputBox}>
+                    <Controller
+                        name="Address"
+                        control={control}
+                        render={({ field: { value } }) => (
+                            <TextInput
+                                style={{ flex: 1 }}
+                                placeholder="Alamat"
+                                onChangeText={(text: string) => setValue('Address', text)}
+                                value={value}
+                            />
+                        )}
+                    />
+                    <FontAwesome6 name="edit" size={24} color="black" />
+                </View>
+                <Text style={styles.errorMessage}>{errors.Address?.message}</Text>
+
+                <View style={styles.inputBox}>
+                    <Controller
+                        name="City"
+                        control={control}
+                        render={({ field: { value } }) => (
+                            <TextInput
+                                style={{ flex: 1 }}
+                                placeholder="Kota"
+                                onChangeText={(text: string) => setValue('City', text)}
+                                value={value}
+                            />
+                        )}
+                    />
+                    <FontAwesome6 name="edit" size={24} color="black" />
+                </View>
+                <Text style={styles.errorMessage}>{errors.City?.message}</Text>
+
+                <View style={styles.inputBox}>
+                    <Controller
+                        name="Province"
+                        control={control}
+                        render={({ field: { value } }) => (
+                            <TextInput
+                                style={{ flex: 1 }}
+                                placeholder="Kota"
+                                onChangeText={(text: string) => setValue('Province', text)}
+                                value={value}
+                            />
+                        )}
+                    />
+                    <FontAwesome6 name="edit" size={24} color="black" />
+                </View>
+                <Text style={styles.errorMessage}>{errors.Province?.message}</Text>
+
+                <View style={styles.inputBox}>
+                    <Controller
+                        name="PostalCode"
+                        control={control}
+                        render={({ field: { value } }) => (
+                            <TextInput
+                                style={{ flex: 1 }}
+                                placeholder="Kode Pos"
+                                onChangeText={(text: string) => {
+                                    const numericValue = parseInt(text);
+                                    if (!isNaN(numericValue)) {
+                                        setValue('PostalCode', numericValue);
+                                    }
+                                }}
+                                value={value?.toString()}
+                                keyboardType="numeric"
+                            />
+                        )}
+                    />
+                    <FontAwesome6 name="edit" size={24} color="black" />
+                </View>
+                <Text style={styles.errorMessage}>{errors.PostalCode?.message}</Text>
 
                 <TouchableOpacity style={styles.button} onPress={handleSubmit(onSubmit)}>
                     <Text className="text-center font-bold text-white">Save</Text>
@@ -221,6 +270,7 @@ const styles = StyleSheet.create({
         backgroundColor: "#CCCCCC",
         padding: 20,
         marginHorizontal: 30,
+        marginBottom: 25,
         borderBottomColor: "#488DF4",
         borderBottomWidth: 2,
         borderRadius: 10,
