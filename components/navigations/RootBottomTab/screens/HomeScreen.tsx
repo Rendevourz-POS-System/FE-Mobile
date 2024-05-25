01496e13cd69f40d81bbf6c114d3cf949eb8b0e4
crislim21 <reavleaz@yahoo.co.id>
2024-05-25 18:56:33 +0700

    Adjust home

--- components/navigations/RootBottomTab/screens/HomeScreen.tsx
+++ components/navigations/RootBottomTab/screens/HomeScreen.tsx
@@ -1,27 +1,8 @@
-import React, { FC, useEffect, useState} from 'react';
-import { FlatList, Image, ScrollView} from 'react-native';
+import React, { FC } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import TopNavigation from '../../../TopNavigation';
-import HomeCarousel from '../../../HomeCarousel';
 import { HomeUser } from '../../HomeUser';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { RootBottomTabCompositeNavigationProp } from '../../CompositeNavigationProps';
-
-interface ShelterData {
-    Id: string;
-    UserId: string;
-    ShelterName: string;
-    ShelterLocation: string;
-    ShelterCapacity: number;
-    ShelterContactNumber: string;
-    ShelterDescription: string;
-    TotalPet: number;
-    BankAccountNumber: string;
-    Pin: string;
-    ShelterVerified: boolean;
-    CreatedAt: string;
-}
-
 
 export const HomeScreen: FC<{}> = ({navigation, route} : any) => {
     const favAttempt = route.params;
